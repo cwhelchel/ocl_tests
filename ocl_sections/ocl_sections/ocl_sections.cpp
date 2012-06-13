bfe729980d988caccd3084b72ce45d4325c5a687
Cainan Whelchel <c.whelchel@gmail.com>
2012-06-13 16:27:02 -0400

    enqueue second kernel for exec in parallel
    This is kinda hacky since the o2o header doesn't handle multiple kernels
    yet

--- ocl_sections/ocl_sections/ocl_sections.cpp
+++ ocl_sections/ocl_sections/ocl_sections.cpp
@@ -11,6 +11,8 @@ const char* kernel2 = "#pragma OPENCL EXTENSION cl_amd_printf : enable \n__kerne
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    cl_int ret;
+
 	o2o_init();
 	o2o_create_cmd_queue(CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE);
 	
@@ -18,10 +20,24 @@ int _tmain(int argc, _TCHAR* argv[])
 	o2o_build_program();
 	o2o_create_kernel("kernel1");
 
-	//o2o_execute_kernel(1);
-    cl_int ret = clEnqueueTask(cmd_q, kernel, 0, NULL, NULL);
+    size_t k_size = strlen(kernel2);
+    cl_program p2 = clCreateProgramWithSource(ocl_ctx, 1, 
+                                    &kernel2, 
+                                    &k_size,
+                                    &ret);
     CHECK(ret);
 
+    ret = clBuildProgram(p2, 1, &d_id, NULL, NULL, NULL);
+    CHECK(ret);
+
+    cl_kernel k2 = clCreateKernel(p2, "kernel2", &ret);
+    CHECK(ret);
+
+    ret = clEnqueueTask(cmd_q, kernel, 0, NULL, NULL);
+    CHECK(ret);
+
+    ret = clEnqueueTask(cmd_q, k2, 0, NULL, NULL);
+    CHECK(ret);
 
 	o2o_finalize();
 	
